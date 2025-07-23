09f93a2a93a35ac9a401f8a55da84821641da5c5
Pickles0 <taylord4062@masseyhigh.school.nz>
2025-07-24 10:25:12 +1200

    Add files via upload

---
+++ PO_2 variables.py
@@ -0,0 +1,15 @@
+# Code to learn about variables...
+
+# Variables can be in a string (note the speech marks)
+greeting = "hello world"
+
+#variables can be a number
+fav_num = 7
+num_string = "5"
+print(fav_num * 2)
+print(num_string * 5)
+
+# constants uppercase
+PI = 3.14
+
+a_01_my_num = 7
